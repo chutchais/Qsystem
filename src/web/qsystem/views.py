abbed31e02131a52e5c98359a0ed9185353a3093
Chutchai Suebklang <chutchai.s@gmail.com>
2020-08-03 17:19:15 +0700

    Change Index page to be stadard HTML(no advance CSS)

--- src/web/qsystem/views.py
+++ src/web/qsystem/views.py
@@ -14,17 +14,59 @@ def Index(request):
 	# today_end = datetime.combine(tomorrow, time())
 	# print(yesterday)
 	fname = "index.html"
+
+	c1=''
+	c2=''
+	c3=''
+	c4=''
+	c5=''
+	c6=''
+	c7=''
+	c8=''
+	c9=''
+	c10=''
+	c11=''
+	c12=''
+	# current_job
 	counter_list = Counter.objects.filter(
 						active=True )
+	for c in counter_list:
+		if c.counter_number==1 :
+			c1 = c.current_job if c.current_job else ''
+		if c.counter_number==2 :
+			c2 = c.current_job if c.current_job else ''
+		if c.counter_number==3 :
+			c3 = c.current_job if c.current_job else ''
+		if c.counter_number==4 :
+			c4 = c.current_job if c.current_job else ''
+		if c.counter_number==5 :
+			c5 = c.current_job if c.current_job else ''
+		if c.counter_number==6 :
+			c6 = c.current_job if c.current_job else ''
+		if c.counter_number==7 :
+			c7 = c.current_job if c.current_job else ''
+		if c.counter_number==8 :
+			c8 = c.current_job if c.current_job else ''
+		if c.counter_number==9 :
+			c9 = c.current_job if c.current_job else ''
+		if c.counter_number==10 :
+			c10 = c.current_job if c.current_job else ''
+		if c.counter_number==11 :
+			c11 = c.current_job if c.current_job else ''
+		if c.counter_number==12 :
+			c12 = c.current_job if c.current_job else ''
+	# print(c1,c2,c3,c4,c5,c6,sc7,c8,c9,c10,c11,c12)
 	return render(
 		request,
 		fname,
 		{
-			'object_list' : counter_list
+
+			'c1':c1,'c2':c2,'c3':c3,'c4':c4,'c5':c5,'c6':c6,
+			'c7':c7,'c8':c8,'c9':c9,'c10':c10,'c11':c11,'c12':c12
 		}
 	)
 	# return render(request, 'index.html')
-
+#			'object_list' : counter_list,
 
 from django.http import JsonResponse
 from django_q.tasks import async_task
